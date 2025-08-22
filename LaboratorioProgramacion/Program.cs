849eef497e524f089e77224798ddef40546944b0
Tiziana <rossi@PCTIZIANA>
2025-08-22 20:26:29 -0300

    Agregar archivos de proyecto.

---
+++ LaboratorioProgramacion/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program {
+    static void Main() {
+        Console.WriteLine("¡Lindo Viernes para estar con Visual Studio 2022!");
+    }
+}
+
+
+
